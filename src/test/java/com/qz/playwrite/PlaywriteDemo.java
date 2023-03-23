f42036f18d47fe59e05647616a96501a836ad5aa
Ratnakar <ratnakar.darapureddi@qualizeal.com>
2023-03-23 17:49:48 +0530

    updated

--- src/test/java/com/qz/playwrite/PlaywriteDemo.java
+++ src/test/java/com/qz/playwrite/PlaywriteDemo.java
@@ -9,13 +9,15 @@ import java.util.regex.Pattern;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class PlaywriteDemo {
+    public static final boolean HEADLESS = true;
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(HEADLESS));
             Page page = browser.newPage();
             page.navigate("https://www.google.com/");
-            System.out.println(page.title());
+            System.out.println("Page title - " + page.title());
             assertThat(page).hasTitle(Pattern.compile("Google"));
+            //assertThat(page).hasTitle(Pattern.compile("Gooooogle "));
             //  page.locator("input[id=\"input\"]").fill("Playwright");
             //  page.locator("input[value=\"Google Search\"]").click();
         }
